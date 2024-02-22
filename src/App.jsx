b43dbe18aebc9ba0469fc7fd2e8a74fc19aa129c
Pamela Santos <pamela-sl@hotmail.com>
2024-02-22 20:43:00 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import './App.css';
+import pokebolaVazia from './assets/images/emptypokeball.png'
+//<img src={require('asset/imagem.png)} />
+
+function App() {
+  
+    function searchPokemon() {
+      const pokemon = document.getElementById("pokemon").value.toLocaleLowerCase();
+      const pokeImg = document.getElementById("pokeImg");
+      const pokeApi = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+
+      fetch(pokeApi)
+      .then(response => response.json())
+      .then(data => {
+        pokeImg.src = data.sprites.front_default;
+        pokeImg.alt = data.name;
+        document.getElementById("pokeName").innerHTML = data.name;
+      })
+      .catch(err => {
+        document.getElementById("pokeName").innerHTML = "Pokémon não encontrado";
+        pokeImg.src = pokebolaVazia;
+        pokeImg.width = 70;
+        pokeImg.alt = "Pokébola aberta e vazia";
+      })
+
+
+    }
+
+    return (
+      <div className="App">
+        <h1>Pokédex</h1>
+        <form>
+          <fieldset>
+            <label htmlFor="pokemon">Pokémon:</label>
+            <input type="text" id="pokemon" placeholder="Digite o nome do Pokémon"/>
+            <button type="button" id="search" onClick={searchPokemon}>Pesquisar Pokémon</button>
+
+            <div id="mostrarPokemon">
+              <h2 id="pokeName"></h2>
+              <img id="pokeImg"/>
+            </div>
+          </fieldset>
+        </form>
+      </div>
+    );
+}
+
+export default App;
